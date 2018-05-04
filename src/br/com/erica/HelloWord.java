1fe6134c70c78d4b0c0ff5d80d1607f2df4ba04a
Erica <ericaborges97@hotmail.com>
2018-05-03 22:21:23 -0300

    "Hello World"

---
+++ src/br/com/erica/HelloWord.java
@@ -0,0 +1,16 @@
+package br.com.erica;
+
+import java.util.Random;
+
+public class HelloWord {
+
+   public static void main(String[] args) {
+        
+        int num = 0;
+        Random ram = new Random();
+        int meuIntAleatorio = ram.nextInt();
+        
+        System.out.println("Hello World");
+    }
+
+}
